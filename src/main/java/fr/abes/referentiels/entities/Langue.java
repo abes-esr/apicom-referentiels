1de5fcc483e150e2acca7f813c13186dc57c0ef8
charot <charot@abes.fr>
2021-10-28 14:42:45 +0200

    Ajout code court pour les langues avec utilisation de la table LANG_ISO_639_2_TO_1

--- src/main/java/fr/abes/referentiels/entities/Langue.java
+++ src/main/java/fr/abes/referentiels/entities/Langue.java
@@ -8,13 +8,14 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity(name="LANG_LABEL")
+@Entity
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Langue {
     @Id
     private String code;
+    private String codecourt;
     private String label;
     private String label_EN;
 }
--- src/main/java/fr/abes/referentiels/repository/LangueRepository.java
+++ src/main/java/fr/abes/referentiels/repository/LangueRepository.java
@@ -2,8 +2,11 @@ package fr.abes.referentiels.repository;
 
 import fr.abes.referentiels.entities.Langue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 
 public interface LangueRepository extends JpaRepository<Langue, String> {
+    @Query( value = "select i.code_1 as codecourt, l.* from LANG_LABEL l left join LANG_ISO_639_2_TO_1 i ON l.code=i.code_2 order by l.label asc", nativeQuery = true)
     List<Langue> findAllByOrderByLabelAsc();
 }
