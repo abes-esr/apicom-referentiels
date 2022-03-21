ed23f456fb98344cac5292f7d1dc1b0e7ac393f4
charot <charot@abes.fr>
2022-03-21 22:39:33 +0100

    Remplace SpringFox par SpringDoc

--- src/main/java/fr/abes/referentiels/ReferentielApplication.java
+++ src/main/java/fr/abes/referentiels/ReferentielApplication.java
@@ -1,5 +1,8 @@
 package fr.abes.referentiels;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,12 +13,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")},
+        info = @Info(title = "Webservice Referentiels Sudoc", version = "1.0", description = "Ce Web Service permet d'obtenir sous forme de liste, en XML ou JSON, un referentiel"))
 public class ReferentielApplication extends SpringBootServletInitializer {
-
-    @Override
-    public SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ReferentielApplication.class);
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ReferentielApplication.class, args);
