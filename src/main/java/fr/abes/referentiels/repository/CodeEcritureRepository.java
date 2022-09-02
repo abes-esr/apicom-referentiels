06be1607229f58be71afc949dd60eaea26c6908e
charot <charot@abes.fr>
2022-09-02 14:57:27 +0200

    Ajout dans le setenv des tomcat de export LANG=french pour corriger le pb d'order by (2)

--- src/main/java/fr/abes/referentiels/repository/CodeEcritureRepository.java
+++ src/main/java/fr/abes/referentiels/repository/CodeEcritureRepository.java
@@ -2,7 +2,6 @@ package fr.abes.referentiels.repository;
 
 import fr.abes.referentiels.entities.CodeEcriture;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
