b78cf2ac730287debcb38795ffdcbff720142eb3
charot <charot@abes.fr>
2022-09-02 11:41:03 +0200

    Test env

--- src/main/java/fr/abes/referentiels/repository/CodeEcritureRepository.java
+++ src/main/java/fr/abes/referentiels/repository/CodeEcritureRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface CodeEcritureRepository extends JpaRepository<CodeEcriture, String> {
-    @Query(value = "SELECT * FROM code_ecriture order by NLSSORT(label,'NLS_SORT=FRENCH')", nativeQuery = true)
+    //@Query(value = "SELECT * FROM code_ecriture order by NLSSORT(label,'NLS_SORT=FRENCH')", nativeQuery = true)
     List<CodeEcriture> findAllByOrderByLabelAsc();
 }
